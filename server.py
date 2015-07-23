521a3ffff631d42573ddfaf748d9d40311c6aafc
mwillcox99 <mwillcox99@gmail.com>
2015-07-23 15:44:48 -0700

    centered

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
- 
+
 web_server = BaseHTTPServer.HTTPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
-web_server.socket = ssl.wrap_socket (web_server.socket, 
+web_server.socket = ssl.wrap_socket (web_server.socket,
                                      server_side=True,
                                      certfile="./server/crt.crt",
                                      keyfile="./server/key.key")
